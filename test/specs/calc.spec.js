fc98f4ef70b9251419547421e6c00e9dfd8c4f42
Steven Strasburg <SSStrasburg@gmail.com>
2017-04-04 00:05:25 -0400

    describe blocks around tests, and removed inital tests

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -2,90 +2,111 @@
     'use strict';
     let expect = chai.expect;
 
-    xit('one equals one test', function() {
-
-      expect(1).to.equal(1);
-    });
-
-    xit('should add numbers', function() {
-      let result = window.calc.sum([ 1, 2, 3]);
-      console.info(result);
-      expect( result ).to.be.a( 'number' ).and.to.equal(6);
-    });
-
-    xit('should add if only one argument', function(){
-      let result = window.calc.sum([55]);
-      expect( result ).to.be.a( 'number' );
-      expect( result ).to.equal(55);
-      // expect( result ).to.be.a( 'number' ).and.to.equal(55);
-    });
 
 
-    xit('should handle no arguments', function(){
-      let result = window.calc.sum();
-      expect( result ).to.equal(0);
-    });
+    describe(
+      it('should add numbers', function() {
+        let result = window.calc.sum([ 1, 2, 3]);
+        console.info(result);
+        expect( result ).to.be.a( 'number' ).and.to.equal(6);
+      });
+    );
 
-    xit('should be NaN if not a array', function(){
-      let result = window.calc.sum('sdfgjhkgfds');
-      expect( result ).to.be.NaN;
-    });
+    describe(
+      it('should add if only one argument', function(){
+        let result = window.calc.sum([55]);
+        expect( result ).to.be.a( 'number' );
+        expect( result ).to.equal(55);
+        // expect( result ).to.be.a( 'number' ).and.to.equal(55);
+      });
+    );
 
-    xit('input should result in NaN if string is not a number', function(){
-      let result = window.calc.sum([2,'foobar',3]);
-      expect(result).to.be.NaN;
-    });
+    describe(
+      it('should handle no arguments', function(){
+        let result = window.calc.sum();
+        expect( result ).to.equal(0);
+      });
+    );
 
-    xit('input should result in NaN if string is not a number', function(){
-      let result = window.calc.sum(['foobar']);
-      expect(result).to.be.NaN;
-    });
+    describe(
+      it('should be NaN if not a array', function(){
+        let result = window.calc.sum('sdfgjhkgfds');
+        expect( result ).to.be.NaN;
+      });
+    );
 
-////
-////
-////
-////factorial portion
-////
+    describe(
+      it('input should result in NaN if string is not a number', function(){
+        let result = window.calc.sum([2,'foobar',3]);
+        expect(result).to.be.NaN;
+      });
+    );
 
-    it('number should output factorial', function(){
-      let result = window.calc.factorial(5);
-      expect(result).to.equal(120);
-    });
+    describe(
+      it('input should result in NaN if string is not a number', function(){
+        let result = window.calc.sum(['foobar']);
+        expect(result).to.be.NaN;
+      });
+    );
 
+  ////
+  ////
+  ////
+  ////factorial portion
+  ////
 
-    it('topNumber should be a number', function(){
-      let result = window.calc.factorial('foobar');
-      expect(result).to.equal(0);
-    });
+    describe(
+      it('number should output factorial', function(){
+        let result = window.calc.factorial(5);
+        expect(result).to.equal(120);
+      });
+    );
 
-    it('if multiple topNumber are inputed only the first number will count as the topNumber', function(){
-      let result = window.calc.factorial(5, 4, 2);
-      expect(result).to.equal(120);
-    });
+    describe(
+      it('topNumber should be a number', function(){
+        let result = window.calc.factorial('foobar');
+        expect(result).to.equal(0);
+      });
+    );
 
-    it('if an array is added it will return 0', function(){
-      let result = window.calc.factorial([ 5, 4, 2]);
-      expect(result).to.equal(0);
-    });
+    describe(
+      it('if multiple topNumber are inputed only the first number will count as the topNumber', function(){
+        let result = window.calc.factorial(5, 4, 2);
+        expect(result).to.equal(120);
+      });
+    );
 
-    // it('taylor series like division of factorial will return 0', function(){
-    //   let result = window.calc.factorial( (factorial(5))/(factorial(4)) );
-    //   expect(result).to.equal(undefined);
-    // });
+    describe(
+      it('if an array is added it will return 0', function(){
+        let result = window.calc.factorial([ 5, 4, 2]);
+        expect(result).to.equal(0);
+      });
+    );
 
-    it('Nonintegers will return 0', function(){
-      let result = window.calc.factorial(1.5);
-      expect(result).to.equal(1);
-    });
+      // it('taylor series like division of factorial will return 0', function(){
+      //   let result = window.calc.factorial( (factorial(5))/(factorial(4)) );
+      //   expect(result).to.equal(undefined);
+      // });
 
-    it('topNumber as 0 will return 1', function(){
-      let result = window.calc.factorial(0);
-      expect(result).to.equal(1);
-    });
+    describe(
+      it('Nonintegers will return 0', function(){
+        let result = window.calc.factorial(1.5);
+        expect(result).to.equal(1);
+      });
+    );
 
-    it('Nonintegers will return 0', function(){
-      let result = window.calc.factorial( -3 );
-      expect(result).to.equal(1);
-    });
+    describe(
+      it('topNumber as 0 will return 1', function(){
+        let result = window.calc.factorial(0);
+        expect(result).to.equal(1);
+      });
+    );
+
+    describe(
+      it('Nonintegers will return 0', function(){
+        let result = window.calc.factorial( -3 );
+        expect(result).to.equal(1);
+      });
+    );
 
 })();
